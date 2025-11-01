0abf2932e16f6f7ff6ba74117276a4db9031f5c0
3x3cut0r <julianreith@gmx.de>
2025-11-01 08:48:18 +0100

    Update PS1.zsh

--- home/PS1.zsh
+++ home/PS1.zsh
@@ -1,7 +1,6 @@
 # Active prompt style
 PS1='%1~ %(!.#.\$) '
 
-# Alternative styles (commented for reference):
-# Style 2: PS1='%~ %(!.#.\$) '
-# Style 3: PS1='%n:%1~ %(!.#.\$) '
-# Style 4: PS1='%n@%m:%1~ %(!.#.\$) '
+# PS1='%~ %(!.#.\$) '
+# PS1='%n:%1~ %(!.#.\$) '
+# PS1='%n@%m:%1~ %(!.#.\$) '

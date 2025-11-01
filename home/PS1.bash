353cfc890ea049fa87139121bbf4b28186a1d26f
3x3cut0r <julianreith@gmx.de>
2025-11-01 08:48:00 +0100

    Comment alternative PS1 styles in PS1.bash
    Comment out alternative prompt styles for clarity.

--- home/PS1.bash
+++ home/PS1.bash
@@ -1,7 +1,6 @@
 # Active prompt style
 PS1="\W \$ "
 
-# Alternative styles (commented for reference):
-# Style 2: PS1="\w \$ "
-# Style 3: PS1="\u:\W \$ "
-# Style 4: PS1="\u@\h:\W \$ "
+# PS1="\w \$ "
+# PS1="\u:\W \$ "
+# PS1="\u@\h:\W \$ "

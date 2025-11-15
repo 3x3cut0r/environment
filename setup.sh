37475163567a557ac51dfc27c7c52c23eb824f8f
3x3cut0r <julianreith@gmx.de>
2025-11-15 17:55:05 +0100

    chore: text

--- setup.sh
+++ setup.sh
@@ -1058,9 +1058,9 @@ configure_terminals() {
     if [ "$AUTO_CONFIRM" = "yes" ]; then
         log_message WARN "Auto confirmation enabled. Configuring terminals without prompt."
     else
-        printf '[Environment][\033[35mINPUT\033[0m] %s' Configure terminals to use JetBrainsMono Nerd Font.
-        printf '[Environment][\033[35mINPUT\033[0m] %s' "This may break your current terminal and you need to restart it afterwards."
-        printf '[Environment][\033[35mINPUT\033[0m] %s' "Continue? [y/N] "
+        printf '[Environment][\033[35mINPUT\033[0m] %s\n' Configure terminals to use JetBrainsMono Nerd Font.
+        printf '[Environment][\033[35mINPUT\033[0m] %s\n' "This may break your current terminal and you need to restart it afterwards."
+        printf '[Environment][\033[35mINPUT\033[0m] %s\n' "Continue? [y/N] "
 
         if [ -t 0 ]; then
             if ! read -r response; then

6a56160c72925cfcd4d1a13b2620ffdc4787f0b4
3x3cut0r <julianreith@gmx.de>
2026-03-23 09:42:47 +0100

    Create which-key.lua

---
+++ home/.config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,59 @@
+return {
+  "folke/which-key.nvim",
+  event = "verylazy",
+  opts = {
+    preset = "helix",
+    delay = 500,
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = true,
+        motions = true,
+        text_objects = true,
+        windows = true,
+        nav = true,
+        z = true,
+        g = true,
+      },
+    },
+    win = {
+      border = "rounded",
+      padding = { 2, 2, 2, 2 },
+    },
+    layout = {
+      width = { min = 20 },
+      spacing = 6,
+    },
+    icons = {
+      breadcrumb = "»",
+      separator = "➜",
+      group = "+",
+    },
+    show_help = true,
+    show_keys = true,
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+
+    wk.add({
+      { "<leader>b", group = "buffer" },
+      { "<leader>c", group = "code" },
+      { "<leader>f", group = "file/find" },
+      { "<leader>g", group = "git" },
+      { "<leader>h", group = "help" },
+      { "<leader>n", group = "notifications" },
+      { "<leader>q", group = "quit/session" },
+      { "<leader>s", group = "search" },
+      { "<leader>t", group = "toggle/terminal" },
+      { "<leader>u", group = "ui" },
+      { "<leader>w", group = "window" },
+      { "<leader>x", group = "diagnostics" },
+    })
+  end,
+}

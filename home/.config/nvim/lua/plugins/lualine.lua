120cda84d74f1ee6fca642355b0605f4cc9f02dd
3x3cut0r <julianreith@gmx.de>
2026-03-24 22:09:44 +0100

    fix: lua catppuccin-nvim

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       options = {
-        theme = "catppuccin",
+        theme = "catppuccin-nvim",
       },
     },
   },

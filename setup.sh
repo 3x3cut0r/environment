82fce11ce9031d98b373549a343c7eaeae8db016
Julian Reith <julianreith@gmx.de>
2026-04-01 17:30:20 +0200

    fix(setup): make quoted command expansion nounset-safe

--- setup.sh
+++ setup.sh
@@ -666,15 +666,11 @@ install_packages() {
             fi
 
             if [ $command_first -eq 1 ]; then
-                if [ ${#quoted_commands[@]} -gt 0 ]; then
-                    run_installer_commands "${quoted_commands[@]}"
-                fi
+                run_installer_commands "${quoted_commands[@]+"${quoted_commands[@]}"}"
                 install_package_segment "$package_segment"
             else
                 install_package_segment "$package_segment"
-                if [ ${#quoted_commands[@]} -gt 0 ]; then
-                    run_installer_commands "${quoted_commands[@]}"
-                fi
+                run_installer_commands "${quoted_commands[@]+"${quoted_commands[@]}"}"
             fi
         done
     fi

c50cca77e63fe4fa7d3d6a5ba3bb0f0c8c121083
Julian Reith <julianreith@gmx.de>
2026-06-15 23:09:29 +0200

    feat: enables qoc + disably helo

--- home/.config/nvim/lua/plugins/coq.lua
+++ home/.config/nvim/lua/plugins/coq.lua
@@ -10,8 +10,11 @@ return {
     lazy = false,
     init = function()
       vim.g.coq_settings = {
-        auto_start = false,
-          display = {
+        auto_start = true,
+        display = {
+          statusline = {
+            helo = false,
+          },
           pum = {
             y_max_len = 8,
             y_ratio = 0.2,

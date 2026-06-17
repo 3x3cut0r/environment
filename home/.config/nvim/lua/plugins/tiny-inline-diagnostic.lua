e1bb8b297890d41ff5a182046c48c68647727c22
Julian Reith <julianreith@gmx.de>
2026-06-17 23:16:57 +0200

    feat: added tiny-inline-diagnostic.lua

---
+++ home/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require("tiny-inline-diagnostic").setup({
+        preset = "modern",
+      })
+
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+    end,
+  },
+}

dd38bf065305958d4080df04b9c226926f87d055
Julian Reith <julianreith@gmx.de>
2026-08-25 23:24:51 +0200

    fix: nvim leader f f now finds hidden files

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -130,7 +130,13 @@ vim.keymap.set("n", "<leader>rt", ":RemoteTreeBrowser ", { desc = "Browse remote
 -- none
 
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
+vim.keymap.set("n", "<leader>ff", function()
+  require("telescope.builtin").find_files({
+    hidden = true,
+    no_ignore = true,
+    find_command = { "fd", "--type", "f", "--hidden", "--no-ignore", "--exclude", ".git", "--exclude", ".svn" },
+  })
+end, { desc = "Find files" })
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help tags" })
 
